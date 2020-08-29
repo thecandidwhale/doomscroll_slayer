3e9aa4956221abbd0ca2e073d2eef8060c4023c5
nate <flyingwithnate@icloud.com>
2020-08-28 20:03:28 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 import tweepy
+from time import sleep
 from secrets import *
 
 auth = tweepy.OAuthHandler(c_key, c_secret)
 auth.set _access_token(a_token, a_token_secret)
 api = tweepy.API(auth)
 
-tweet = "This is a friendly reminder to make sure you're not doomscrolling."
+tweet_text = "This is a friendly reminder to make sure you're not doomscrolling."
 
-api.update_status(tweet)
+def tweet():
+        for line in file_lines:
+            try:
+                api.update_status(tweet_text)
+                sleep(1800)
+
+tweet()
