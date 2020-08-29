7080255c841adfa1f01ec47aa6d64e69be0c8c6c
nate <flyingwithnate@icloud.com>
2020-08-28 20:13:05 -0700

    tweet file
    added file to pull tweet from to clean up code.
    removed draft code. need to continue working on this later.

--- bot.py
+++ bot.py
@@ -3,15 +3,5 @@ from time import sleep
 from secrets import *
 
 auth = tweepy.OAuthHandler(c_key, c_secret)
-auth.set _access_token(a_token, a_token_secret)
+auth.set_access_token(a_token, a_token_secret)
 api = tweepy.API(auth)
-
-tweet_text = "This is a friendly reminder to make sure you're not doomscrolling."
-
-def tweet():
-        for line in file_lines:
-            try:
-                api.update_status(tweet_text)
-                sleep(1800)
-
-tweet()
