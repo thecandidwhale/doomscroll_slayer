ac65eaa68296a8c60f38439d80af844ff2403bb7
nate <flyingwithnate@icloud.com>
2020-08-30 19:29:09 -0700

    tweets "Hello World!"

--- bot.py
+++ bot.py
@@ -4,4 +4,9 @@ from secrets import *
 
 auth = tweepy.OAuthHandler(c_key, c_secret)
 auth.set_access_token(a_token, a_token_secret)
-api = tweepy.API(auth)
+
+#print("hello world") #bot functions to this point
+
+twitter = tweepy.API(auth)
+
+twitter.update_status("Hello World!")
